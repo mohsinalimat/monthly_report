85a85c02480308d0a807c2a59d80a25cd6f77e0e
Farabi Hussain <farabi.hussain@sgatechsolutions.com>
2022-11-03 14:29:34 -0500

    WWEMP-47 Monthly Financial Statements

--- monthly_report/monthly_report/report/monthly_income_statement/monthly_income_statement.js
+++ monthly_report/monthly_report/report/monthly_income_statement/monthly_income_statement.js
@@ -45,8 +45,8 @@ frappe.query_reports["Monthly Income Statement"] = {
 			label: __("Print Group"),
 			fieldtype: "Select",
 			options: [
-				{ "value": "Weekly", "label": __("Weekly") },
-				{ "value": "Monthly", "label": __("Monthly") }
+				{ "value": "Weekly" },
+				{ "value": "Monthly" }
 			],
 			reqd: 1,
 			get_data: function (txt) {
